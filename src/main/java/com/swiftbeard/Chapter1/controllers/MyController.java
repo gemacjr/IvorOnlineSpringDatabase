050601dc574a165b1d3144878c23334e13711aa2
gemacjr <edmccormic@me.com>
2024-09-16 16:01:31 -0400

    Person RUD repository

--- src/main/java/com/swiftbeard/Chapter1/controllers/MyController.java
+++ src/main/java/com/swiftbeard/Chapter1/controllers/MyController.java
@@ -17,63 +17,9 @@ import java.util.List;
 public class MyController {
     //PROPERTIES
     @Autowired PersonRepository personRepository;
-    @RequestMapping("NoParameters")
-    Person noParameters() {
-        Person person = personRepository.noParameters();
-        return person;
-    }
-    //================================================================
-// INDEXED PARAMETERS
-//================================================================
-    @RequestMapping("IndexedParameters")
-    Person indexedParameters() {
-        Person person = personRepository.indexedParameters("John", 20);
-        return person;
-    }
-    //================================================================
-// NAMED PARAMETERS
-//================================================================
-    @RequestMapping("NamedParameters")
-    Person namedParameters() {
-        Person person = personRepository.namedParameters("John", 20);
-        return person;
-    }
-    //================================================================
-// SELECT PERSON BY NAME AGE (Returns Single Entity)
-//================================================================
     @RequestMapping("SelectPersonByNameAge")
     Person selectPersonByNameAge() {
-        Person john = personRepository.selectPersonByNameAge("John", 20);
-        return john;
-    }
-    //================================================================
-// SELECT PERSONS BY NAME (Returns List)
-//================================================================
-    @RequestMapping("SelectPersonsByName")
-    List<Person> selectPersonsByName() {
-        List<Person> persons = personRepository.selectPersonsByName("John");
-        return persons;
-    }
-
-    @RequestMapping("UpdatePersonsByName")
-    String updatePersonsByName() {
-        Integer recordsUpdated = personRepository.updatePersonsByName("John", 50); //New age
-        return recordsUpdated + " Records Updated";
-    }
-    //================================================================
-// DELETE PERSON BY NAME
-//================================================================
-    @RequestMapping("DeletePersonsByName")
-    String deletePersonsByName() {
-        Integer recordsDeleted = personRepository.deletePersonsByName("John");
-        return recordsDeleted + " Records Deleted";
-    }
-    //=======================================================================================
-// INSERT PERSON
-//=======================================================================================
-    @RequestMapping("InsertPerson")
-    String insertPerson() {
-        Integer recordsDeleted = personRepository.insertPerson("Jack", 100);
-        return recordsDeleted + " Records Inserted";
+        Person person = personRepository.selectPersonByNameAge("John", 20); //Returns Single Entity
+        return person;
     }
 }
--- src/main/java/com/swiftbeard/Chapter1/repositories/PersonRepository.java
+++ src/main/java/com/swiftbeard/Chapter1/repositories/PersonRepository.java
@@ -10,45 +10,8 @@ import java.util.List;
 
 public interface PersonRepository extends CrudRepository<Person, Integer> {
     //=======================================================================================
-// PARAMETERS
+// SELECT PERSON BY NAME AGE
 //=======================================================================================
-//NO PARAMETERS
-    @Query(value = "SELECT * FROM PERSON WHERE NAME = 'John' AND AGE = 20", nativeQuery = true)
-    Person noParameters();
-    //INDEXED PARAMETERS
-    @Query(value = "SELECT * FROM PERSON WHERE NAME = ?1 AND AGE = ?2", nativeQuery = true)
-    Person indexedParameters(String name, Integer age);
-    //NAMED PARAMETERS
-    @Query(value = "SELECT * FROM PERSON WHERE NAME = :name AND AGE = :parameterAge", nativeQuery = true)
-    Person namedParameters(String name, @Param("parameterAge") Integer age);
-    //=======================================================================================
-// SELECT
-//=======================================================================================
-//RETURN SINGLE ENTITY
     @Query(value = "SELECT * FROM PERSON WHERE NAME = :name AND AGE = :age", nativeQuery = true)
     Person selectPersonByNameAge(String name, Integer age);
-    //RETURN LIST
-    @Query(value = "SELECT * FROM PERSON WHERE NAME = :name", nativeQuery = true)
-    List<Person> selectPersonsByName(String name);
-    //=======================================================================================
-// UPDATE
-//=======================================================================================
-    @Modifying
-    @Transactional
-    @Query(value = "UPDATE PERSON SET AGE = :newAge WHERE NAME = :name", nativeQuery = true)
-    Integer updatePersonsByName(String name, Integer newAge);
-    //=======================================================================================
-// DELETE
-//=======================================================================================
-    @Modifying
-    @Transactional
-    @Query(value = "DELETE FROM PERSON WHERE NAME = :name", nativeQuery = true)
-    Integer deletePersonsByName(String name);
-    //=======================================================================================
-// INSERT
-//=======================================================================================
-    @Modifying
-    @Transactional
-    @Query(value = "INSERT INTO PERSON (name, age) VALUES (:name, :age)", nativeQuery = true)
-    Integer insertPerson(String name, Integer age);
 }
